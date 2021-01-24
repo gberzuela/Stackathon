225535851ece47f50bf183686900941ecf61ac64
Gerald Lou Berzuela <berzuelagerald3348@gmail.com>
2021-01-24 10:06:32 -0800

    ADD: link flow plaid routes

---
+++ server/api/plaid.js
@@ -0,0 +1,69 @@
+const router = require('express').Router()
+const plaid = require('plaid')
+module.exports = router
+
+console.log('process.env stuff ********** ->', process.env.PLAID_ENV)
+
+const client = new plaid.Client({
+  clientID: process.env.PLAID_CLIENT_ID,
+  secret: process.env.PLAID_SECRET,
+  env: plaid.environments[process.env.PLAID_ENV]
+})
+
+let ACCESS_TOKEN = null
+let PUBLIC_TOKEN = null
+let ITEM_ID = null
+
+router.post('/create_link_token', async (req, res, next) => {
+  try {
+    // Get client user id
+    // const user = req.user
+    // const clientUserId = user.id.toString()
+    const client_user_id = '1'
+
+    // Create link-token
+    const tokenResponse = await client.createLinkToken({
+      user: {
+        client_user_id
+      },
+      client_name: 'Stackathon',
+      products: ['transactions'],
+      country_codes: ['US'],
+      language: 'en'
+    })
+
+    res.send(tokenResponse.link_token)
+  } catch (error) {
+    next(error)
+  }
+})
+
+router.post('/get_access_token', async (req, res, next) => {
+  PUBLIC_TOKEN = req.body.token
+  client.exchangePublicToken(PUBLIC_TOKEN, function(error, tokenResponse) {
+    if (error) {
+      res.send(error)
+      return
+    }
+    res.sendStatus(200)
+    ACCESS_TOKEN = tokenResponse.access_token
+    ITEM_ID = tokenResponse.item_id
+    res.json({
+      accessToken: ACCESS_TOKEN,
+      itemId: ITEM_ID
+    })
+    console.log('access token below')
+    console.log(ACCESS_TOKEN)
+  })
+  // const response = await client.exchangePublicToken(PUBLIC_TOKEN)
+  // ACCESS_TOKEN = response.access_token
+  // ITEM_ID = response.item_id
+  // res.send({ACCESS_TOKEN, ITEM_ID})
+})
+
+router.get('/accounts', (req, res, next) => {
+  client.getAccounts(ACCESS_TOKEN, (err, accountsResponse) => {
+    if (err) res.send(err)
+    res.send({error: null, accounts: accountsResponse})
+  })
+})
