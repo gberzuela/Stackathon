b620e08260da3652da65a30f062ede791c00dc46
Gerald Lou Berzuela <berzuelagerald3348@gmail.com>
2021-01-25 09:56:33 -0800

    STYLE: basic css for table

--- client/components/Accounts.js
+++ client/components/Accounts.js
@@ -1,36 +1,36 @@
 import React from 'react'
 import {connect} from 'react-redux'
 
+import '../style/Table.css'
+
 const Accounts = props => {
   const {accounts} = props
   // console.log('Accounts component -->', accounts)
   // accounts.forEach((account) => console.log(account))
   return (
-    <table>
-      <thead>
-        <tr>
-          <th>Accounts</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <th>Name</th>
-          <th>Balance</th>
-        </tr>
-        {accounts.length ? (
-          accounts.map(account => (
-            <tr key={account.id}>
-              <td>{account.name}</td>
-              <td>{account.balance ? account.balance : 0}</td>
-            </tr>
-          ))
-        ) : (
+    <div>
+      Accounts
+      <table>
+        <tbody>
           <tr>
-            <td>No data. Try syncing.</td>
+            <th>Name</th>
+            <th>Balance</th>
           </tr>
-        )}
-      </tbody>
-    </table>
+          {accounts.length ? (
+            accounts.map((account, idx) => (
+              <tr key={account.id} className={idx % 2 ? 'grey' : ''}>
+                <td>{account.name}</td>
+                <td>{account.balance ? account.balance : 0}</td>
+              </tr>
+            ))
+          ) : (
+            <tr>
+              <td>No data. Try syncing.</td>
+            </tr>
+          )}
+        </tbody>
+      </table>
+    </div>
   )
 }
 
--- client/components/Transactions.js
+++ client/components/Transactions.js
@@ -2,39 +2,39 @@ import React from 'react'
 import {connect} from 'react-redux'
 import moment from 'moment'
 
+import '../style/Table.css'
+
 const Transactions = props => {
   const {transactions} = props
   console.log('Transactions component -->', transactions)
   return (
-    <table>
-      <thead>
-        <tr>
-          <th>Transactions</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <th>Name</th>
-          <th>Category</th>
-          <th>Date</th>
-          <th>Amount</th>
-        </tr>
-        {transactions.length ? (
-          transactions.map(transaction => (
-            <tr key={transaction.id}>
-              <td>{transaction.name}</td>
-              <td>{transaction.category.join(', ')}</td>
-              <td>{moment(transaction.date).format('YYYY-MM-DD')}</td>
-              <td>{transaction.amount}</td>
-            </tr>
-          ))
-        ) : (
+    <div>
+      Transactions
+      <table>
+        <tbody>
           <tr>
-            <td>No data. Try syncing.</td>
+            <th>Name</th>
+            <th>Category</th>
+            <th>Date</th>
+            <th>Amount</th>
           </tr>
-        )}
-      </tbody>
-    </table>
+          {transactions.length ? (
+            transactions.map((transaction, idx) => (
+              <tr key={transaction.id} className={idx % 2 ? 'grey' : ''}>
+                <td>{transaction.name}</td>
+                <td>{transaction.category.join(', ')}</td>
+                <td>{moment(transaction.date).format('YYYY-MM-DD')}</td>
+                <td>{transaction.amount}</td>
+              </tr>
+            ))
+          ) : (
+            <tr>
+              <td>No data. Try syncing.</td>
+            </tr>
+          )}
+        </tbody>
+      </table>
+    </div>
   )
 }
 
